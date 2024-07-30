4c387eef65a412cbc2a04a11a4415a0af48a9dc3
alesha74111 <alesha74111@gmail.com>
2024-07-30 15:42:07 +0300

    animation, js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,44 @@ const refs = {
     openMobileBtn: document.querySelector("[data-mobile-open]"),
     closeMobileBtn: document.querySelector("[data-mobile-close]"),
     mobileMenu: document.querySelector("[data-mobile-menu]"),
+    // closeMenu: document.querySelectorAll('.js-close-menu')
+    closeMenu: document.querySelectorAll(".menu__close")
     };
-
-refs.openMobileBtn.addEventListener("click", toggleModal);
-refs.closeMobileBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-    // document.body.classList.toggle("data-mobile-menu");
+    refs.closeMenu.forEach(menu__close => {
+            menu__close.addEventListener('click', toggleModal);});
+        
+    refs.openMobileBtn.addEventListener("click", toggleModal);
+    refs.closeMobileBtn.addEventListener("click", toggleModal);
+    // refs.closeMenu.addEventListener('click', toggleModal);
+    refs.mobileMenu.addEventListener('click', removeModal);
+    
+    function toggleModal() {
+    document.body.classList.toggle("no-scroll");
     refs.mobileMenu.classList.toggle("is-open");
     }
 
+    function removeModal(e) {
+        if (e.target === refs.mobileMenu) {
+            refs.mobileMenu.classList.toggle('is-open');
+            document.body.classList.toggle("no-scroll");
+        }
+    }
+    //animation
+    function onEntry(entry) {
+        entry.forEach(change => {
+    if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+    }
+    });
+    }
+    const options = {
+        threshold: [0.5] };
+    const observer = new IntersectionObserver(onEntry, options);
+    const elements = document.querySelectorAll('.element-animation');
+    for (const elm of elements) {
+        observer.observe(elm);
+    }
+// scroll
     window.onscroll = function () {
         scrollFunction();
     };
